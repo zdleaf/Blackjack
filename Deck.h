f9b7f26e9b3ca819c72f772c86e4b015bc702fe0
ZDLeaf <zach@zincldn.co.uk>
2020-03-10 14:04:38 +0000

    filled Deck.h with basic stack template

--- Deck.h
+++ Deck.h
@@ -3,12 +3,26 @@
 
 using namespace std;
 
-
-class Deck{
-    private:
+template <typename T>
+class Deck {
+    private: 
+        std::deque<T> stack; // internal representation of the Stack
 
     public:
+        // return the top element of the Stack
+        T& top(){ return stack.front(); }
+            
+        // push an element onto the Stack
+        void push(const T pushValue){ stack.push_front(pushValue); }
+        
+        // pop an element from the stack
+        void pop() { stack.pop_front(); }
 
+        // determine whether Stack is empty
+        bool isEmpty() const { return stack.empty(); }
+        
+        // return size of Stack
+        size_t size() const { return stack.size(); }
 };
 
 
