9a2238c855dd2f36cc43a17b529fd19c1b43ce21
ZDLeaf <zach@zincldn.co.uk>
2020-04-14 15:37:44 +0100

    cleaning up Blackjack.h/correct folder

--- include/Blackjack/Blackjack.h
+++ include/Blackjack/Blackjack.h
@@ -2,16 +2,15 @@
 #define BLACKJACK_H
 
 #include <iostream>
-#include <iomanip>
-#include <random>
-#include <sstream>
-#include <tuple>
 
 #include "Deck.h"
 #include "Cards.h"
 
 class Blackjack{
     private:
+        Deck<Card> *deck;
+        Card *card1;
+        Card *card1;
 
     public:
 };
